5d1e12f49a09a12b9a57d6032e3ac0854bad1f64
lyncredulous <caitlyn.king2014@gmail.com>
2022-01-03 22:03:55 +0000

    Add further functionality and debug

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let computed = false;
+
 function main() {
   const display = document.querySelector(".screen p");
   const buttons = Array.from(document.querySelectorAll("button"));
@@ -9,6 +11,7 @@ function main() {
 function operate(vals) {
   let result = Number(vals[0]);
   let operator = "";
+  computed = true;
 
   for (let i = 1; i <= vals.length; i++) {
     if (isNaN(vals[i])) {
@@ -65,16 +68,37 @@ function divide(x, y) {
 }
 
 function updateDisplay(btn, display) {
+  //Don't allow operators or decimals to be typed before numbers
+  if (display.textContent === "" && btn.classList.contains("operator") || display.textContent === "" && btn.innerText === ".") {
+      return
+  }
+  // Allows user to use decimal button. Reenables button once an operator is pressed.
+  if (btn.innerText === ".") {
+    btn.disabled = true;
+  } else if (btn.classList.contains("operator")) {
+    document.getElementById("decimal").disabled = false;
+  }
+
+  //Allow user to clear screen
   if (btn.innerText === "c") {
     display.textContent = "";
   } else if (btn.innerText === "=") {
-    const operation = Array.from(
-      display.innerText.split(/(\d+|[^\d]+)/g).filter(Boolean)
-    );
+    const operation = display.innerText.match(/[\d\.]+|\D+/g);
     let result = operate(operation);
     display.textContent = result;
   } else {
-    display.textContent += btn.innerText;
+    // Will clear screen when button is pressed after calculation unless it's an operator
+    if (computed === true) {
+      if (btn.classList.contains("operator") || btn.innerText === ".") {
+        display.textContent += btn.innerText;
+        computed = false;
+      } else {
+        display.textContent = btn.innerText;
+        computed = false;
+      }
+    } else {
+      display.textContent += btn.innerText;
+    }
   }
 }
 
