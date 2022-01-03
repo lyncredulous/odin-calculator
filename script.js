31700a8c3679d4a52cf0cf2efc8e1203af262326
lyncredulous <caitlyn.king2014@gmail.com>
2022-01-03 19:39:11 +0000

    Add base functions

---
+++ script.js
@@ -0,0 +1,23 @@
+function main() {
+
+}
+
+function operate(x, y) {
+
+}
+
+function add(x, y) {
+    return x + y
+}
+
+function subtract(x, y) {
+    return x - y
+}
+
+function multiply(x, y) {
+    return x * y
+}
+
+function divide(x, y) {
+    return x / y
+}
